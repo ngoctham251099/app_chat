31ba5f082723224ee73aaf3818f743c78bfc7b8b
ngoctham251099 <thamt8974@gmail.com>
2021-06-30 21:50:45 +0700

    test merge to conflic

--- appchat/pages/user.tsx
+++ appchat/pages/user.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 
 const user = () => {
+  const a = 10;
   return (
     <div>
       <h1>Hello users</h1>
       <h2>adssad</h2>
+      <h2>{a}</h2>
     </div>
   )
 }
 
-export default user
+export default user;
